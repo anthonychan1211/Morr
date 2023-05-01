5b255e6fc77d68bf2c21d0c5e8ac34d5b6b8f414
Long Yin Chan <anthony@longs-mbp.home>
2023-05-01 19:23:09 +0100

    final

--- pages/api/updateOrderStatus.ts
+++ pages/api/updateOrderStatus.ts
@@ -14,7 +14,7 @@ export default async function addToUserBag(
         id: order_id,
       },
       data: {
-        tracking_number,
+        tracking_number: tracking_number || null,
         order_status,
       },
     });
